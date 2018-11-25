611f19531af91354dcb238739436650508fa58ca
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-11-25 13:27:11 +0100

    Recover amplify project

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,11 +2,14 @@
 
 const awsmobile =  {
     "aws_project_region": "eu-west-1",
-    "aws_cognito_identity_pool_id": "eu-west-1:81b91f47-f163-49d7-8980-48d00fc56ea6",
+    "aws_content_delivery_bucket": "amplifyexperiments-20181125131341--hostingbucket",
+    "aws_content_delivery_bucket_region": "eu-west-1",
+    "aws_content_delivery_url": "http://amplifyexperiments-20181125131341--hostingbucket.s3-website-eu-west-1.amazonaws.com",
+    "aws_cognito_identity_pool_id": "eu-west-1:37965605-00d2-414a-9690-e1eacb6766b0",
     "aws_cognito_region": "eu-west-1",
-    "aws_user_pools_id": "eu-west-1_efRtGWMw8",
-    "aws_user_pools_web_client_id": "7c1to76fmmnb42be9hg9n3pe2a",
-    "aws_appsync_graphqlEndpoint": "https://g7ipbyx555aglcqnmaoue6lrwe.appsync-api.eu-west-1.amazonaws.com/graphql",
+    "aws_user_pools_id": "eu-west-1_1XauCSRCA",
+    "aws_user_pools_web_client_id": "5aj6aedod5bjpnervh72m2t4h5",
+    "aws_appsync_graphqlEndpoint": "https://jpawtc67nbcq7nv3id5tzy6t3y.appsync-api.eu-west-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-1",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
