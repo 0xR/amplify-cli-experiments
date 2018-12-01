45bf9e21b7b28cfba766496e8223f29f0cd3d9cc
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-12-01 16:09:46 +0100

    call appsync from react

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react';
-import Amplify from 'aws-amplify';
+import React, { Component } from "react";
+import "./App.css";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify from "aws-amplify";
 
-import aws_exports from './aws-exports';
+import aws_exports from "./aws-exports";
+import { ApolloProvider, Query } from "react-apollo";
+import { appsyncClient } from "./appsyncClient";
+import { ListProductsQuery, ListProductsQueryVariables } from "./API";
+import { listProducts } from "./graphql/queries";
+import gql from "graphql-tag";
+import { notEmpty } from "./ts-utils";
+
 Amplify.configure(aws_exports);
+
+class ProductListQuery extends Query<
+  ListProductsQuery,
+  ListProductsQueryVariables
+> {}
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={appsyncClient}>
+        <ProductListQuery query={gql(listProducts)}>
+          {({ data, loading, error }) => {
+            if (loading) {
+              return "Loading...";
+            }
+            if (data && data.listProducts && data.listProducts.items) {
+              return (
+                <ul>
+                  {data.listProducts.items.filter(notEmpty).map(({ title, id }) => (
+                    <li key={id}>{title}</li>
+                  ))}
+                </ul>
+              );
+            }
+          }}
+        </ProductListQuery>
+      </ApolloProvider>
     );
   }
 }
---
+++ src/appsyncClient.ts
@@ -0,0 +1,13 @@
+import Amplify, { Auth } from 'aws-amplify';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import aws_config from './aws-exports';
+
+export const appsyncClient = new AWSAppSyncClient({
+  url: aws_config.aws_appsync_graphqlEndpoint,
+  region: aws_config.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
+  }
+});
+
