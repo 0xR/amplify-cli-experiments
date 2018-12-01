cb2531205d0fc017c5095d22805c75ee7de08054
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-12-01 22:43:36 +0100

    Detect isEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React, { Component, useEffect } from "react";
-import "./App.css";
-import { withAuthenticator } from "aws-amplify-react";
-import Amplify, { Auth } from "aws-amplify";
+import React, { Component, useEffect, useState } from 'react';
+import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import Amplify, { Auth, Cache } from 'aws-amplify';
 
-import aws_exports from "./aws-exports";
-import { ApolloProvider, Query } from "react-apollo";
-import { appsyncClient } from "./appsyncClient";
-import { ListProductsQuery, OnStockSubscription } from "./API";
-import Async from "react-promise";
-import { notEmpty } from "./ts-utils";
-import { listProductsQuery, stockSubscription } from "./App.gql";
-import { Router, Switch, Route, Redirect, Link } from "react-router-dom";
-import createBrowserHistory from "history/createBrowserHistory";
+import aws_exports from './aws-exports';
+import { ApolloProvider, Query } from 'react-apollo';
+import { appsyncClient } from './appsyncClient';
+import { ListProductsQuery, OnStockSubscription } from './API';
+import Async from 'react-promise';
+import { notEmpty } from './ts-utils';
+import { listProductsQuery, stockSubscription } from './App.gql';
+import { Router, Switch, Route, Redirect, Link } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 const history = createBrowserHistory();
 
-(Async as any).defaultPending = () => "Loading...";
+(Async as any).defaultPending = () => 'Loading...';
 
 Amplify.configure(aws_exports);
 
@@ -27,27 +27,46 @@ type NotEmptyArray<T> = T extends Array<infer T2>
 
 interface ProductListProps {
   products: NotEmptyArray<
-    NonNullable<ListProductsQuery["listProducts"]>["items"]
+    NonNullable<ListProductsQuery['listProducts']>['items']
   >;
 
-  subscriptionEffect: () => () => void
+  subscriptionEffect: () => () => void;
 }
 
-const LoginPage2 = () => <Redirect to="/products"/>;
+const LoginPage2 = () => <Redirect to="/products" />;
 
 const LoginPage1 = withAuthenticator(LoginPage2);
 
-const LoginPage = () => <Async
-  promise={Auth.signOut()}
-  then={() => <LoginPage1/>}
-/>;
+const LoginPage = () => (
+  <Async promise={Auth.signOut()} then={() => <LoginPage1 />} />
+);
+
+function useIsEditor() {
+  const [isEditor, setIsEditor] = useState(false);
+
+  useEffect(() => {
+    Auth.currentSession().then(session => {
+      const groups =
+        session.getAccessToken().decodePayload()['cognito:groups'] || [];
+      const newIsEditor = groups.includes('editor');
+      if (newIsEditor !== isEditor) {
+        setIsEditor(newIsEditor);
+      }
+    });
+  });
+
+  return isEditor;
+}
 
 const ProductList = ({ products, subscriptionEffect }: ProductListProps) => {
   useEffect(subscriptionEffect);
+  const isEditor = useIsEditor();
   return (
     <ul>
       {products.map(({ title, id, stock }) => (
-        <li key={id}>{title} ({stock} left)</li>
+        <li key={id}>
+          {title} ({stock} left) {isEditor && 'editor'}
+        </li>
       ))}
     </ul>
   );
@@ -58,12 +77,17 @@ class ProductListPage extends Component {
     return (
       <Async
         promise={Auth.currentUserPoolUser().catch(() =>
-          Auth.signIn("guest", "Welcom1!")
+          Auth.signIn('guest', 'Welcom1!')
         )}
-        then={({ username }) => {
+        then={({ username, ...userInfo }) => {
+          console.log({
+            userInfo
+          });
           return (
             <>
-              <p>Signed in as {username} <Link to="/login">change user</Link></p>
+              <p>
+                Signed in as {username} <Link to="/login">change user</Link>
+              </p>
               <ApolloProvider client={appsyncClient}>
                 <ProductListQuery query={listProductsQuery}>
                   {({ data, loading, error, subscribeToMore }) => {
@@ -71,7 +95,7 @@ class ProductListPage extends Component {
                       return `Error: ${error}`;
                     }
                     if (loading) {
-                      return "Loading data...";
+                      return 'Loading data...';
                     }
                     if (data && data.listProducts && data.listProducts.items) {
                       return (
@@ -80,27 +104,36 @@ class ProductListPage extends Component {
                             subscribeToMore({
                               document: stockSubscription,
                               updateQuery: (prev, { subscriptionData }) => {
-                                const subscriptionREsult = subscriptionData.data as any as OnStockSubscription | null;
-                                if(!prev.listProducts || !subscriptionREsult || !subscriptionREsult.onUpdateProduct) {
+                                const subscriptionREsult = (subscriptionData.data as any) as OnStockSubscription | null;
+                                if (
+                                  !prev.listProducts ||
+                                  !subscriptionREsult ||
+                                  !subscriptionREsult.onUpdateProduct
+                                ) {
                                   return prev;
                                 }
-                                const newStockId = subscriptionREsult.onUpdateProduct.id;
-                                const { listProducts, listProducts: { items }} = prev;
-                                if(!items) {
+                                const newStockId =
+                                  subscriptionREsult.onUpdateProduct.id;
+                                const {
+                                  listProducts,
+                                  listProducts: { items }
+                                } = prev;
+                                if (!items) {
                                   return prev;
                                 }
                                 return {
                                   ...prev,
                                   listProducts: {
                                     ...listProducts,
-                                    items: items.map((product) => {
-                                      if(!product) {
+                                    items: items.map(product => {
+                                      if (!product) {
                                         return product;
                                       }
                                       return {
                                         ...product,
-                                          ...product.id === newStockId && subscriptionREsult.onUpdateProduct
-                                      }
+                                        ...(product.id === newStockId &&
+                                          subscriptionREsult.onUpdateProduct)
+                                      };
                                     })
                                   }
                                 };
@@ -122,12 +155,20 @@ class ProductListPage extends Component {
   }
 }
 
-const App = () => <Router history={history}>
-  <Switch>
-    <Route path="/products"><ProductListPage/></Route>
-    <Route path="/login"><LoginPage/></Route>
-    <Route><Redirect to="/products"/></Route>
-  </Switch>
+const App = () => (
+  <Router history={history}>
+    <Switch>
+      <Route path="/products">
+        <ProductListPage />
+      </Route>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <Route>
+        <Redirect to="/products" />
+      </Route>
+    </Switch>
   </Router>
+);
 
 export default App;
